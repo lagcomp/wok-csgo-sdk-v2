c468b314738db2328a3d39a9a9c2204e4e1da3a4
sove <13748681+sowwe@users.noreply.github.com>
2020-08-20 23:08:47 +0300

    Update globals.cpp

--- wok-csgo/globals.cpp
+++ wok-csgo/globals.cpp
@@ -1,13 +1,1 @@
-#include "globals.h"
 
-namespace g {
-	namespace angles {
-		qangle_t    view = {};
-		qangle_t    real = {};
-	}
-
-	HMODULE			module = nullptr;
-	c_local_player	local = {};
-	c_user_cmd*		cmd = nullptr;
-	bool			send_packet = true;
-}
