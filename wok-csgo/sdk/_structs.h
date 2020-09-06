e631e803d498484e5cc9f0a88be44c7e3ec55a92
sove <13748681+sowwe@users.noreply.github.com>
2020-09-06 13:06:02 +0300

    Update _structs.h

--- wok-csgo/sdk/_structs.h
+++ wok-csgo/sdk/_structs.h
@@ -246,6 +246,35 @@ enum e_invalidate_physics_bits {
 	SEQUENCE_CHANGED 	= (1 << 5)
 };
 
+enum e_cmd_button {
+	IN_ATTACK			= (1 << 0),
+	IN_JUMP				= (1 << 1),
+	IN_CROUCH			= (1 << 2),
+	IN_FORWARD			= (1 << 3),
+	IN_BACK				= (1 << 4),
+	IN_USE				= (1 << 5),
+	IN_CANCEL			= (1 << 6),
+	IN_LEFT				= (1 << 7),
+	IN_RIGHT				= (1 << 8),
+	IN_MOVE_LEFT		= (1 << 9),
+	IN_MOVE_RIGHT		= (1 << 10),
+	IN_ATTACK2			= (1 << 11),
+	IN_RUN				= (1 << 12),
+	IN_RELOAD			= (1 << 13),
+	IN_ALT1				= (1 << 14),
+	IN_ALT2				= (1 << 15),
+	IN_SCORE				= (1 << 16),
+	IN_SPEED				= (1 << 17),
+	IN_WALK				= (1 << 18),
+	IN_ZOOM				= (1 << 19),
+	IN_WEAPON1			= (1 << 20),
+	IN_WEAPON2			= (1 << 21),
+	IN_BULLRUSH			= (1 << 22),
+	IN_GRENADE_LEFT	= (1 << 23),
+	IN_GRENADE_RIGHT	= (1 << 24),
+	IN_LOOK_SPIN		= (1 << 25)
+};
+
 enum e_cvar_flags {
 	FCVAR_NONE,
 	FCVAR_UNREGISTERED				= (1 << 0),
