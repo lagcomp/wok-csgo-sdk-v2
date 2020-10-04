f74895a11ad5d203496a123c45db6157c1b2d451
sove <13748681+sowwe@users.noreply.github.com>
2020-10-05 01:52:03 +0300

    Update events.cpp

--- wok-csgo/events/events.cpp
+++ wok-csgo/events/events.cpp
@@ -1,15 +1,15 @@
 #include "events.h"
 
 namespace events {
-	void c_listener::fire_game_event(i_game_event* event) { m_callbacks.at(FNV1A_RT(event->get_name()))(event); }
+	__forceinline void c_listener::fire_game_event(i_game_event* event) { m_callbacks.at(FNV1A_RT(event->get_name()))(event); }
 
-	__forceinline void add(const char* name, std::function<void(i_event*)> fn) {
+	__forceinline void add_callback(const char* name, std::function<void(i_event*)> fn) {
 		m_callbacks[FNV1A_RT(name)] = fn;
 		interfaces::event_manager->add_listener(&m_listener, _(name), false);
 	}
 	
 	__forceinline void init() {
-		add("player_hurt", player_hurt);
+		add_callback("player_hurt", player_hurt);
 	}
 
 	__forceinline void undo() { interfaces::event_manager->remove_listener(&m_listener); }
