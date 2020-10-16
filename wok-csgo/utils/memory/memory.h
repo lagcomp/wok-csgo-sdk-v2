c867a2aef285bf3f5489f94e69b4a3b499350efe
sove <13748681+sowwe@users.noreply.github.com>
2020-10-16 21:31:24 +0300

    Update memory.h

--- wok-csgo/utils/memory/memory.h
+++ wok-csgo/utils/memory/memory.h
@@ -117,7 +117,9 @@ namespace memory {
 		template <typename T>
 		__forceinline T cast() const { return reinterpret_cast<T>(m_ptr); }
 
-		__forceinline uint8_t* offset(uint32_t value) const { return m_ptr + value; };
+		__forceinline uint8_t* offset(ptrdiff_t value) const {
+			return m_ptr + value;
+		}
 
 		__forceinline address_t& self_offset(uint32_t value) {
 			m_ptr += value;
@@ -125,9 +127,26 @@ namespace memory {
 			return *this;
 		}
 
-		__forceinline uint8_t* jmp(uint32_t offset = 0x1) const { return m_ptr + offset + sizeof(uintptr_t) + *reinterpret_cast<int*>(m_ptr + offset); }
+		__forceinline uint8_t* jmp(ptrdiff_t offset = 0x1) const {
+			/// Base address is the address that follows JMP (0xE9) instruction.
+			auto base = m_ptr + offset;
 
-		__forceinline address_t& self_jmp(uint32_t offset = 0x1) {
+			/// Store the displacement
+			/// Note: Displacement addresses can be signed, thanks d3x.
+			auto displacement = *reinterpret_cast<int*>(base);
+
+			/// The JMP is based on the instruction after the address
+			/// so the address size has to be added
+			/// Note: This is always 4 bytes, regardless of architecture, thanks d3x
+			base += sizeof(uintptr_t);
+
+			/// Now finally do the JMP by adding the function address
+			base += displacement;
+
+			return base;
+		}
+
+		__forceinline address_t& self_jmp(ptrdiff_t offset = 0x1) {
 			m_ptr = jmp(offset);
 
 			return *this;
