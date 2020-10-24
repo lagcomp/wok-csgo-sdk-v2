b6f6dfbf720cdfb7190d749d660bd0ca8c8f08c3
sove <13748681+sowwe@users.noreply.github.com>
2020-10-24 22:07:08 +0300

    Update panel.cpp

--- wok-csgo/hooks/vtables/panel.cpp
+++ wok-csgo/hooks/vtables/panel.cpp
@@ -3,9 +3,21 @@
 void __fastcall hooks::panel::paint_traverse::fn(void* ecx, void* edx, uint32_t id, bool force_repaint, bool allow_force) {
 	static const auto original = m_panel->get_original<T>(index);
 
-	if (HASH_RT(interfaces::panel->get_name(id)) == HASH("MatSystemTopPanel")) {
-		render::begin();
-	}
+	original(ecx, id, force_repaint, allow_force);
 
-	original(interfaces::panel, id, force_repaint, allow_force);
+	{
+		static auto draw_panel_id = 0;
+
+		if (!draw_panel_id) {
+			if (strcmp(interfaces::panel->get_name(id), _("MatSystemTopPanel")))
+				return;
+
+			draw_panel_id = id;
+		}
+
+		if (id != draw_panel_id)
+			return;
+	}
+	
+	render::begin();
 }
