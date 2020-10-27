bc493a5857255b14de7ba566ac7c064878dac0d1
lagcomp <40600834+lagcomp@users.noreply.github.com>
2020-10-27 22:20:32 +0300

    Update i_game_types.h

--- wok-csgo/sdk/interfaces/i_game_types.h
+++ wok-csgo/sdk/interfaces/i_game_types.h
@@ -2,9 +2,9 @@
 
 class i_game_types {
 public:
-	VFUNC(get_current_game_type(), 8, int(__thiscall*)(void*))
-	VFUNC(get_current_game_mode(), 9, int(__thiscall*)(void*))
-	VFUNC(get_current_map_name(), 10, const char*(__thiscall*)(void*))
-	VFUNC(get_current_game_type_name_id(), 11, const char*(__thiscall*)(void*))
-	VFUNC(get_current_game_mode_name_id(), 13, const char*(__thiscall*)(void*))
+	VFUNC(get_cur_game_type(), 8, int(__thiscall*)(void*))
+	VFUNC(get_cur_game_mode(), 9, int(__thiscall*)(void*))
+	VFUNC(get_cur_map_name(), 10, const char*(__thiscall*)(void*))
+	VFUNC(get_cur_game_type_name_id(), 11, const char*(__thiscall*)(void*))
+	VFUNC(get_cur_game_mode_name_id(), 12, const char*(__thiscall*)(void*))
 };
