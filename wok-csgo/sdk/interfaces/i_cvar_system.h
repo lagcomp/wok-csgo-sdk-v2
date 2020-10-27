1820bd6c79c1f8ee836f82a51629d2ca6cda7c4d
DerLi <63970543+pDerli@users.noreply.github.com>
2020-10-27 16:35:33 +0200

    Update i_cvar_system, i_engine_client, add i_game_types, del x64
    Yes...

--- wok-csgo/sdk/interfaces/i_cvar_system.h
+++ wok-csgo/sdk/interfaces/i_cvar_system.h
@@ -1,6 +1,7 @@
 #pragma once
 
 using create_interface_fn = uintptr_t*(__cdecl*)(const char* name, int* code);
+using change_callback_fn = void(__cdecl*)(void*, const char*, float);
 
 class i_app_system {
 public:
@@ -23,6 +24,10 @@ public:
 	virtual void			unregister_cmd(c_cvar* cvar) = 0;
 	virtual void			unregister_cmds(int id) = 0;
 	virtual c_cvar*			find_var(const char* name) = 0;
+	virtual c_cvar*			find_command(const char* name) = 0;
+	virtual void			install_global_change_callback(change_callback_fn callback) = 0;
+	virtual void			remove_global_change_callback(change_callback_fn callback) = 0;
+	virtual void			call_global_change_callbacks(c_cvar* cvar, const char* old_string, float old_value) = 0;
 
 	template <typename... A>
 	VFUNC(console_print(const col_t& clr, const char* txt, A... args), 25, void(__cdecl*)(void*, const col_t&, const char*, ...), clr, txt, args...)
